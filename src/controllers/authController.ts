796a88c8c768840215980be65158d976c43321ea
Davi Castro <santos.davi2110@gmail.com>
2022-12-13 15:01:04 -0300

    feat: route to register a new user was added

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,11 +1,47 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcryptjs';
+import { prisma } from '../libs/prisma';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
-export function register(req: Request, res: Response) {
-  res.json({
-    message: 'ok',
-  });
+import { InternalServerError } from '../errors/InternalServerError';
+import { InvalidRequestError } from '../errors/InvalidRequestError';
+
+export async function register(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const userData = req.body;
+  const password = bcrypt.hashSync(userData.password, 10);
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: userData.name,
+        email: userData.email,
+        password,
+      },
+    });
+
+    const token = jwt.sign({ sub: user.id }, process.env.JWT_KEY as string, {
+      expiresIn: '7 days',
+    });
+
+    res.status(201).json({
+      message: 'user created with sucess.',
+      statusCode: 201,
+      data: {
+        token,
+      },
+    });
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      if ((err.code = 'P2002'))
+        return next(new InvalidRequestError('E-mail already registered.'));
+    }
+    next(new InternalServerError());
+  }
 }
 
 export function me(req: Request, res: Response) {}
---
+++ src/errors/InternalServerError.ts
@@ -0,0 +1,7 @@
+export class InternalServerError extends Error {
+  public statusCode: number;
+  constructor() {
+    super('Internal server error.');
+    this.statusCode = 400;
+  }
+}
