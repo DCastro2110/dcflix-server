ffb4b34a2feb7f80165191f1c733d0d5c43a61ca
Davi Castro <santos.davi2110@gmail.com>
2022-12-16 11:26:41 -0300

    refactor: error messages in registerSchema was changed

--- src/schemas/registerSchema.ts
+++ src/schemas/registerSchema.ts
@@ -3,12 +3,18 @@ import * as yup from 'yup';
 export const registerSchema = yup.object().shape({
   body: yup.object().shape({
     name: yup.string().required(),
-    email: yup.string().email().required(),
+    email: yup
+      .string()
+      .email('The format of email is invalid.')
+      .required('Email field cannot be empty.'),
     password: yup
       .string()
-      .required()
-      .min(8)
-      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/),
+      .required('Password field cannot be empty.')
+      .min(8, 'The password must have at least 8 characters.')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/,
+        'The format of password is invalid.'
+      ),
     passwordConfirmation: yup
       .string()
       .required()
